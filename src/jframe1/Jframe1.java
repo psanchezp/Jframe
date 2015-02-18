5d8a503312c21c8bdfbd7d2fbc48b5ea95901946
Patricio Sanchez <partricios94@gmail.com>
2015-02-18 11:13:34 -0600

    Commit Inicial
    Commit Inicial de Clase de Videojuegos

---
+++ src/jframe1/Jframe1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jframe1;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
+
+/**
+ *
+ * @author Patricio Sanchez
+ */
+public class Jframe1{
+    /**
+     * Clase main del programa
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
---
+++ src/jframe1/Juego.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jframe1;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
+/**
+ *
+ * @author Patricio
+ */
+public class Juego extends JFrame{
+    /**
+     * Constructor del Juego
+     */
+    public Juego(){
+        //Nombre del Juego
+        setTitle("Juego JFrame1");
+        //Cerrando el proceso
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Tamaño de la ventana
+        
+    }
+    
+    public void paint(){
+        
+    }
+}
